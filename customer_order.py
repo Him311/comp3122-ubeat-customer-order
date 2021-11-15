ec3e9bef3501c559c63c736c2b17f9e25562d92b
him <18062182A@student.hkcc-polyu.edu.hk>
2021-11-15 21:03:45 +0800

    [feat] add message_queue

--- customer_order.py
+++ customer_order.py
@@ -1,8 +1,10 @@
 import flask
 import pymongo
+import redis
 
 flask_app = flask.Flask(__name__)
 mongo_client = pymongo.MongoClient('mongodb://comp3122:23456@customer_order_db:27017', serverSelectionTimeoutMS=2000)
+redis_conn = redis.Redis(host='message_queue', port=6379)
 
 db = mongo_client["customer_orders"]
 col = db["Order"]
@@ -19,5 +21,7 @@ def get_order(order_id):
                 return x, 200
 
 if __name__ == '__main__':
+    redis_pubsub = redis_conn.pubsub()
+    redis_pubsub_thread = redis_pubsub.run_in_thread(sleep_time=0.001)
     flask_app.run(host='0.0.0.0', debug=True, port=15000)
 
