d5a4cf30eaf1f16917eb714bc20309a479677266
him <18062182A@student.hkcc-polyu.edu.hk>
2021-11-15 20:59:03 +0800

    [feat] add get order_id

--- customer_order.py
+++ customer_order.py
@@ -7,6 +7,17 @@ mongo_client = pymongo.MongoClient('mongodb://comp3122:23456@customer_order_db:2
 db = mongo_client["customer_orders"]
 col = db["Order"]
 
+@flask_app.route('/<order_id>', methods=['GET'])
+def get_order(order_id):
+    check = col.find_one({'order.order_id': order_id}, { '_id': 0})
+    if check is None:
+        return {'error': 'not found'}, 404
+    else:
+        order = col.find_one({'order.order_id': order_id}, { '_id': 0})['order']
+        for x in order:
+            if x['order_id'] == order_id:
+                return x, 200
+
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0', debug=True, port=15000)
 
