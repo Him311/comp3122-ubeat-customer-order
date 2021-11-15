e455adf70ba2559f6579e71cac18e52eaf6277bf
him <18062182A@student.hkcc-polyu.edu.hk>
2021-11-15 21:13:32 +0800

    [feat] make test_get_order

--- test/unit.py
+++ test/unit.py
@@ -1,3 +1,13 @@
+import redis
+import requests
 
+redis_conn = redis.Redis(host='message_queue', port=6379)
+
+def test_get_order():
+    response = requests.get('http://customer_order:15000/r1o1')
+    assert response.status_code == 200
+    assert response.json() == {
+        'order_id':'r1o1', 'restaurant_id':1, 'food_id':1, 'taken':1
+    }
 
     
